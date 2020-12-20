5b0f4f468a1810b5208e1f9a415721efd1de095d
sumant mishra <sumantmishra511@gmail.com>
2020-12-20 21:11:41 +0530

    video design change
    cards added
    
    angle down chngse
    
    design change
    
    changes in cards
    
    price added
    
    pricing snap removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent
+    CarouselComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule,
