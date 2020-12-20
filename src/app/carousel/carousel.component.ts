5bfd00d4a5aaf9885e742f671a674a4a64a28842
sumant mishra <sumantmishra511@gmail.com>
2020-12-20 15:51:29 +0530

    why fitbox added

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { LABELS } from "../labels.constants";
 
 @Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  selector: "app-carousel",
+  templateUrl: "./carousel.component.html",
+  styleUrls: ["./carousel.component.css"],
 })
 export class CarouselComponent implements OnInit {
+  labels = LABELS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/labels.constants.ts
@@ -0,0 +1,6 @@
+export const LABELS = {
+    CAROUSLE_HEADING: "Learn Anytime...Anywhere",
+    CAROUSLE_TITLE:
+      "As we are bringing learning to people instead of people to learning.",
+  };
+  
