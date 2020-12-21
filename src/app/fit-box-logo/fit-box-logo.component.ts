dbc2c1d5b23d571366d705b42d93dc849e5b74e4
sumant mishra <sumantmishra511@gmail.com>
2020-12-21 20:22:34 +0530

    changes in images

---
+++ src/app/fit-box-logo/fit-box-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fit-box-logo',
+  templateUrl: './fit-box-logo.component.html',
+  styleUrls: ['./fit-box-logo.component.css']
+})
+export class FitBoxLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
