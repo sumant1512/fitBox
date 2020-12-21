a59ce3abad477f4de5b7e5b82b869980124a6672
sumant mishra <sumantmishra511@gmail.com>
2020-12-21 20:22:34 +0530

    changes in images
    video added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { FitBoxLogoComponent } from './fit-box-logo/fit-box-logo.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CarouselComponent } from "./carousel/carousel.component";
+import { PricingComponent } from "./pricing/pricing.component";
+import { FitBoxLogoComponent } from "./fit-box-logo/fit-box-logo.component";
+import { WhyFitBoxComponent } from "./why-fit-box/why-fit-box.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     CarouselComponent,
     PricingComponent,
-    FitBoxLogoComponent
+    FitBoxLogoComponent,
+    WhyFitBoxComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
