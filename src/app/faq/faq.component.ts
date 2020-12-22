1024ee6d5356d7522c1d966dd63f358623442869
sumant mishra <sumantmishra511@gmail.com>
2020-12-22 23:22:05 +0530

    design chages
    fit menu added
    
    testimonial added
    
    animation added
    
    design changes
    
    color change
    
    design change
    
    padding removed
    
    bottom line changed
    
    faq added
    
    design change
    
    design change
    
    footer added

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-faq',
-  templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
+  selector: "app-faq",
+  templateUrl: "./faq.component.html",
+  styleUrls: ["./faq.component.css"],
 })
 export class FaqComponent implements OnInit {
+  faqList = [
+    { question: "Tour Video", mp4Video: "mov_bbb.mp4", oggVideo: "mov_bbb.ogg" },
+    {
+      question: "Pay-Per-Workout",
+      mp4Video: "mov_bbb.mp4",
+      oggVideo: "mov_bbb.ogg",
+    },
+    { question: "Fit-Switch", mp4Video: "mov_bbb.mp4", oggVideo: "mov_bbb.ogg" },
+    {
+      question: "Fit-Transfer",
+      mp4Video: "mov_bbb.mp4",
+      oggVideo: "mov_bbb.ogg",
+    },
+    {
+      question: "Fit-Friends",
+      mp4Video: "mov_bbb.mp4",
+      oggVideo: "mov_bbb.ogg",
+    },
+    { question: "Fit-Family", mp4Video: "mov_bbb.mp4", oggVideo: "mov_bbb.ogg" },
+    { question: "Fit-Focus", mp4Video: "mov_bbb.mp4", oggVideo: "mov_bbb.ogg" },
+    { question: "Fit-Happy", mp4Video: "mov_bbb.mp4", oggVideo: "mov_bbb.ogg" },
+    { question: "Fit-Train", mp4Video: "mov_bbb.mp4", oggVideo: "mov_bbb.ogg" },
+    { question: "Fit-GroupX", mp4Video: "mov_bbb.mp4", oggVideo: "mov_bbb.ogg" },
+    {
+      question: "Fit-Welcome",
+      mp4Video: "mov_bbb.mp4",
+      oggVideo: "mov_bbb.ogg",
+    },
+    { question: "Fit-Test", mp4Video: "mov_bbb.mp4", oggVideo: "mov_bbb.ogg" },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
