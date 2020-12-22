4a6d5540d77c43aae3efa9142e5d743b384f3ff9
sumant mishra <sumantmishra511@gmail.com>
2020-12-22 14:53:55 +0530

    changes in design

--- src/app/fit-freedom/fit-freedom.component.ts
+++ src/app/fit-freedom/fit-freedom.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-fit-freedom',
-  templateUrl: './fit-freedom.component.html',
-  styleUrls: ['./fit-freedom.component.css']
+  selector: "app-fit-freedom",
+  templateUrl: "./fit-freedom.component.html",
+  styleUrls: ["./fit-freedom.component.css"],
 })
 export class FitFreedomComponent implements OnInit {
+  activeCss: boolean = false;
+  lineCss: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    console.log(window.scrollY);
+    if (window.scrollY >= 1638) {
+      this.activeCss = true;
+      setTimeout(() => {
+        this.lineCss = true;
+      }, 1000);
+    }
   }
-
 }
--- src/app/why-fit-box/why-fit-box.component.ts
+++ src/app/why-fit-box/why-fit-box.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  OnInit,
+  Output,
+} from "@angular/core";
+import { LABELS } from "../labels.constants";
 
 @Component({
-  selector: 'app-why-fit-box',
-  templateUrl: './why-fit-box.component.html',
-  styleUrls: ['./why-fit-box.component.css']
+  selector: "app-why-fit-box",
+  templateUrl: "./why-fit-box.component.html",
+  styleUrls: ["./why-fit-box.component.css"],
 })
 export class WhyFitBoxComponent implements OnInit {
+  @Output() scrollPage = new EventEmitter<string>();
+  labels = LABELS;
+  activeCss: boolean = false;
+  lineCss: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    console.log(window.scrollY);
+    if (window.scrollY >= 819) {
+      this.activeCss = true;
+      setTimeout(() => {
+        this.lineCss = true;
+      }, 1000);
+    }
   }
 
+  scrollToPage(selectedPage: string) {
+    console.log(selectedPage);
+    this.scrollPage.emit(selectedPage); // this emits the toggle status to parent component so that it can open or close the navigation accordingly.
+  }
 }
